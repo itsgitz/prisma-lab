ba677d1632c9a77d1bc04674396c49a5cb6d7c3f
itsgitz <anggit.ginanjar.dev@gmail.com>
2024-11-27 10:56:13 +0700

    chore: added prisma scripts

---
+++ src/create-user.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // create user
+  const user = await prisma.user.create({
+    data: {
+      name: 'Anggit',
+      email: 'anggit@prisma.io',
+    },
+  })
+  console.log(user)
+}
+
+main()
+.then(async () => {
+  await prisma.$disconnect()
+})
+.catch(async (e) => {
+  console.error(e)
+  await prisma.$disconnect()
+  process.exit(1)
+})
